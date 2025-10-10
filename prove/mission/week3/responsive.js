c9ee3a03d1e04feedb4ecb8331914b924aa62c4a
Calvin55b <rop22006@byui.edu>
2025-10-10 12:29:48 -0600

    I added photos and js to my photo gallery website.

---
+++ prove/mission/week3/responsive.js
@@ -0,0 +1,46 @@
+const gallery = document.querySelector('.all_my_photos');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+let btn = document.querySelector('.menu-button');
+
+const menu = document.querySelectorAll('nav a');
+
+btn.addEventListener('click', showNav);
+    
+    function showNav(){
+
+        menu.forEach(link => link.classList.toggle("show"));
+        console.log("works");
+
+        
+    }
+
+
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e){
+    console.log(e.target);
+
+    const img = e.target
+
+    const src = img.getAttribute('src');
+
+    const alt = img.getAttribute('alt');
+
+    const full = src.replace('sm', 'full')
+
+    modalImage.src = full;
+
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+        modal.close();
+    })
+
+
