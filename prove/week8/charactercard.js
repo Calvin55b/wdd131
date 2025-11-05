2e6d66fb30a7d079cff06137b49a7090ab87ae6c
Calvin55b <rop22006@byui.edu>
2025-11-05 12:28:12 -0700

    I was able to add My css and html !

---
+++ prove/week8/charactercard.js
@@ -0,0 +1,20 @@
+const cardStats = {
+
+    class: 'Swamp Master',
+
+    lifeAndLevel: [
+        {life:0}, {level:0}
+    ],
+
+    Life: function (life) {
+
+
+    
+    }
+       
+
+    }
+
+
+
+
