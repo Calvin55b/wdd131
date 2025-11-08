8751b91913912f1a356bb877f17308aa58a9e9ce
Calvin55b <rop22006@byui.edu>
2025-11-08 15:18:44 -0700

    I changed a few things

--- prove/week8/charactercard.js
+++ prove/week8/charactercard.js
@@ -1,19 +1,82 @@
+const health = document.querySelector('#health');
+const level = document.querySelector('#level');
+
+
+
+
+// health.addEventListener("click", function(){
+
+//     console.log("health");
+//     health.
+// })
+// level.addEventListener("click", function(){
+
+//     console.log("level");
+// })
+
+
 const cardStats = {
 
     class: 'Swamp Master',
 
-    lifeAndLevel: [
-        {life:0}, {level:0}
-    ],
+    sections: [ {label:'Health', value: 10}, {label: 'level', value: 0}
 
-    Life: function (life) {
+    ]
+
+};
+
+
+function sectionTemplate(section) {
+    return `
+    <tr>
+
+      <td>${section.label}:</td>
+      <td>${section.value}</td>
+      
+      </tr>
+      `;
+
+
+}
+
+
+function renderSections(sections) {
+const html = sections.map(sectionTemplate);
+document.querySelector("#sections").innerHTML = html.join("");
+}
+    
+
+renderSections(cardStats.sections);
+
+
+ health.addEventListener("click", function() {
+      cardStats.sections[0].value -= 1; 
+      if (cardStats.sections[0].value < 0) {
+
+        cardStats.sections[0].value = 10;
+        alert("🎃 You Died!");
+      }
+      renderSections(cardStats.sections);
+    });
+    
+ level.addEventListener("click", function() {
+      cardStats.sections[0].value += 1; 
+      renderSections(cardStats.sections);
+    });
+    
+    
+    
+
+    
+
+        
 
 
     
-    }
+    
        
 
-    }
+    
 
 
 
